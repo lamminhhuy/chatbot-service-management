4034472d6b3281b0dcd239b93fee37483d68fbd6
minhhuylam <lhuy00429@gmail.com>
2025-03-23 15:23:06 +0700

    fix : wrong import in app file

--- src/main/app.ts
+++ src/main/app.ts
@@ -7,15 +7,15 @@ import { errorHandler } from "@/shared/middlewares/error/errorHanlder";
 import cookieParser from "cookie-parser";
 import  morgan  from 'morgan';
 import { env } from "@/configs/envConfig";
-import promptRouter from "@/modules/prompt/routes/promptRoutes";
 import chatRouter from "@/modules/chatbot/routes/chatRoutes";
 import { mongoDBInstance } from "@/database/MongoDB";
 import { initializeDatabase } from "@/database/PostgresDB";
 import { pingServer } from "@/shared/utils/ping";
 import * as cron from 'node-cron';
-import { userRouter } from "@/modules/user/routes/UserRoute";
 import { redisInstance } from "@/database/redisClient";
-import { authRouter } from "@/modules/auth/routes/AuthRouter";
+// import promptRouter from "@/modules/prompt/routes/promptRoutes";
+// import { userRouter } from "@/modules/user/routes/UserRoute";
+// import { authRouter } from "@/modules/auth/routes/AuthRouter";
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
@@ -39,9 +39,9 @@ mongoDBInstance;
 redisInstance;
 
 app.use(`/api/v1/chat`, chatRouter);
-app.use(`/api/v1/prompt`, promptRouter);
-app.use(`/api/v1/user`, userRouter);
-app.use(`/api/v1/auth`, authRouter);
+// app.use(`/api/v1/prompt`, promptRouter);
+// app.use(`/api/v1/user`, userRouter);
+// app.use(`/api/v1/auth`, authRouter);
 
 app.use(errorHandler);
 
