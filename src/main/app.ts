535cb4fcc4ee86b3e23457b04ad47df51d3881db
minhhuylam <lhuy00429@gmail.com>
2025-03-30 15:52:29 +0700

    fix: middware cors message log

--- src/main/app.ts
+++ src/main/app.ts
@@ -30,7 +30,7 @@ app.use(
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(new Error(`${origin} Not allowed by CORS`));
       }},
     credentials: true,
   })
