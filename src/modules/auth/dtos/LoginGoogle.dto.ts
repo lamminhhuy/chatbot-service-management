c2dd9f2c3011a09fb7e50e66b7a3a755a463432b
minhhuylam <lhuy00429@gmail.com>
2025-05-08 19:37:18 +0700

    fix: LoginGoogleDTOSchema

--- src/modules/auth/dtos/LoginGoogle.dto.ts
+++ src/modules/auth/dtos/LoginGoogle.dto.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const LoginGoogleDTOSchema = z.object({
-    token: z.string().min(1).max(1000)
+    token: z.string().min(1)
 });
 
 export type LoginGoogleDTO = z.infer<typeof LoginGoogleDTOSchema>;
