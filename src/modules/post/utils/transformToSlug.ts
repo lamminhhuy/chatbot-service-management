5579ed845c219504ade4e9f1ea2256da36e22c74
minhhuylam <lhuy00429@gmail.com>
2025-05-10 02:13:16 +0700

    fix: transform to slug logic

--- src/modules/post/utils/transformToSlug.ts
+++ src/modules/post/utils/transformToSlug.ts
@@ -1,4 +1,7 @@
-export const transformToSlug = ( title: string, fullSlugCategory?: string): { spacedString?: string, slug: string } => {
+export const transformToSlug = (
+    title: string,
+    fullSlugCategory?: string
+): { spacedString?: string; slug: string } => {
     const removeVietnameseTones = (str: string): string => {
         return str
             .normalize('NFD')
@@ -7,29 +10,32 @@ export const transformToSlug = ( title: string, fullSlugCategory?: string): { sp
             .replace(/Đ/g, 'D');
     };
 
-   
     const spacedString = fullSlugCategory
         ?.split('/')
-        .map(segment => removeVietnameseTones(segment)
-            .replace(/[^a-zA-Z0-9\s]/g, ' ')
-            .replace(/\s+/g, ' ')           
-            .trim())                        
+        .map(segment =>
+            segment
+                .replace(/[^a-zA-Z0-9\u00C0-\u1EF9\s]/g, ' ') 
+                .replace(/\s+/g, ' ')
+                .trim()
+        )
         .join('/');
 
     const categorySlug = fullSlugCategory
         ?.split('/')
-        .map(segment => removeVietnameseTones(segment)
-            .toLowerCase()
-            .replace(/[^a-z0-9]/g, '-')     
-            .replace(/-{2,}/g, '-')         
-            .replace(/^-+|-+$/g, ''))       
+        .map(segment =>
+            removeVietnameseTones(segment)
+                .toLowerCase()
+                .replace(/[^a-z0-9]/g, '-')
+                .replace(/-{2,}/g, '-')
+                .replace(/^-+|-+$/g, '')
+        )
         .join('/');
 
     const titleSlug = removeVietnameseTones(title)
         .toLowerCase()
-        .replace(/[^a-z0-9]/g, '-')         
-        .replace(/-{2,}/g, '-')             
-        .replace(/^-+|-+$/g, '');           
+        .replace(/[^a-z0-9]/g, '-')
+        .replace(/-{2,}/g, '-')
+        .replace(/^-+|-+$/g, '');
 
     const slug = fullSlugCategory ? `${categorySlug}/${titleSlug}` : titleSlug;
 
