0fc8ae8c13d33d5fab65784d5da9ac40f09321e4
minhhuylam <lhuy00429@gmail.com>
2025-05-10 12:10:23 +0700

    fix: update post category schema

--- src/modules/post/dtos/UpdatePostCategory.dto.ts
+++ src/modules/post/dtos/UpdatePostCategory.dto.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const UpdatePostCategoryDTOSchema = z.object({
     name: z.string(),
     friendlySlug: z.string(),
-    parentId: z.number().optional()
+    parentId: z.null()
 });
 
 export type UpdatePostCategoryDTOType = z.infer<typeof UpdatePostCategoryDTOSchema>;
