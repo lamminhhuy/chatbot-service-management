d40f4ce45db9b1c9829fc743af94fba2687f9078
minhhuylam <lhuy00429@gmail.com>
2025-06-09 19:36:51 +0700

    fix: email service

--- src/infrastructure/email/EmailService.ts
+++ src/infrastructure/email/EmailService.ts
@@ -1,34 +1,60 @@
-import { injectable } from "tsyringe";
-import nodemailer from 'nodemailer'
-import { env } from '@/configs/envConfig'
+import { env } from '@/configs/envConfig';
+import { IEmailService } from '@/infrastructure/email/interfaces/IEmailService';
+import nodemailer, { SendMailOptions } from 'nodemailer';
+import { injectable } from 'tsyringe';
 
 @injectable()
 export class EmailService implements IEmailService {
   private transporter;
+
   constructor() {
-    this.transporter = nodemailer.createTransport({
+    if (!env.EMAIL_USER || !env.EMAIL_PASSWORD) {
+      throw new Error('Missing EMAIL_USER or EMAIL_PASSWORD in environment variables');
+    }
+
+    console.log('Email user:', env.EMAIL_USER);
+    console.log('Email password:', env.EMAIL_PASSWORD);
+
+    this.transporter = this.createTransporter();
+  }
+
+  private createTransporter() {
+    return nodemailer.createTransport({
       host: 'smtp.gmail.com',
       port: 587,
-      secure: false,
+      secure: false, 
       auth: {
         user: env.EMAIL_USER,
-        pass: env.EMAIL_PASSWORD
+        pass: env.EMAIL_PASSWORD,
       },
       tls: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false, 
       },
       debug: true,
-      logger: true
+      logger: true,
     });
   }
 
-  async send(mailOptions: nodemailer.SendMailOptions): Promise<void> {
+  async send(mailOptions: SendMailOptions): Promise<void> {
     try {
       await this.transporter.sendMail(mailOptions);
-      console.log('Email sent successfully');
+      console.log('Email sent successfully:', mailOptions.to);
     } catch (error) {
       console.error('Error sending email:', error);
-      throw new Error(`Failed to send email: ${error.message}`);
+      if (error.message.includes('Unexpected socket close')) {
+        console.log('Reinitializing transporter due to socket close');
+        this.transporter = this.createTransporter();
+        try {
+          await this.transporter.sendMail(mailOptions);
+          console.log('Email sent successfully after reinitializing transporter:', mailOptions.to);
+        } catch (retryError) {
+          console.error('Retry failed:', retryError);
+          throw new Error(`Failed to send email after retry: ${retryError.message}`);
+        }
+      } else {
+        throw new Error(`Failed to send email: ${error.message}`);
+      }
     }
   }
+
 }
